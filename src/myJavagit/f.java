ed81cc3d1eaff9f3cb4f8aec7fd1c047431d0992
kotsolesya <kots.olesya@gmail.com>
2015-10-26 15:47:46 +0200

    2 java commit

--- src/myJavagit/f.java
+++ src/myJavagit/f.java
@@ -3,6 +3,10 @@ package myJavagit;
 public class f {
 
 	   public static void main(String args[]) {
-	        System.out.println("Hello, World!");
+	        System.out.println("Yes, I did it:)");
+	        int a=5;
+	        double b=5.3;
+	        if (a>b) { System.out.println("max="+a); }
+	        else { System.out.println("max="+b);}
 	    }
 }
